cb9506987cf9a1dea45f5b6cac67fc8a513a8635
MANOJ PALI <manojpali.x@outlook.com>
2025-06-25 15:09:26 +0530

    path is revert back to login/

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
 ]
