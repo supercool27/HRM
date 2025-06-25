7d55e17ec6e3239ea41ceb5a63ebe0d28e31cdb5
MANOJ PALI <manojpali.x@outlook.com>
2025-06-25 16:31:36 +0530

    def index is added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("✅ /accounts/ is working!")
 
 urlpatterns = [
+    path('', index),  # Test root
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
