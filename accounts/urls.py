edb946a0309e0cb97f733e404b92231b699e9eea
MANOJ PALI <manojpali.x@outlook.com>
2025-06-25 15:53:35 +0530

    modify path is added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
