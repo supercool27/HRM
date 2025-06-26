d5470a60dedee40d220a1f123a235f9aaf1ae1a4
MANOJ PALI <manojpali.x@outlook.com>
2025-06-26 14:14:39 +0530

    testing root in railway environment

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('accounts/', include('accounts.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('', TemplateView.as_view(template_name='accounts/login.html')),
 ]
