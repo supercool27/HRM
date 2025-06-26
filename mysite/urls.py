09d64f3cdf6f11fd7a361b7c9d96663ace852c9c
MANOJ PALI <manojpali.x@outlook.com>
2025-06-26 13:36:42 +0530

    Fix: direct url for view

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('accounts.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
