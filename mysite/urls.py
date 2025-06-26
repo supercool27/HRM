82527e1981d7206bee4457b17297be41ac4a0654
MANOJ PALI <manojpali.x@outlook.com>
2025-06-26 16:08:41 +0530

    checking for live site pre setting why the error is coming not inlcuding template

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,6 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('accounts/', include('accounts.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('', TemplateView.as_view(template_name='accounts/login.html')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),  # ✅ Required
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),  # Optional but useful
 ]
