52566b95bc90652ae0ba408be23e4b1700bf12c6
MANOJ PALI <manojpali.x@outlook.com>
2025-06-26 13:39:48 +0530

    using only login word for login url

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('accounts/', include('accounts.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
