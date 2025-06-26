8e61f145f6df1fe287a97a05c63063581799af5a
MANOJ PALI <manojpali.x@outlook.com>
2025-06-26 15:54:18 +0530

    procfile is added

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,14 +55,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 UI_TEMPLATES = os.path.join(BASE_DIR, 'templates')
 
